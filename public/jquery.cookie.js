42aea0f310123795bd70ed737239e72bce5df4b6
hasen j <hasan.aljudy@gmail.com>
2012-01-15 00:42:49 -0800

    fix cookies

--- public/jquery.cookie.js
+++ public/jquery.cookie.js
@@ -58,11 +58,12 @@
  * @author Klaus Hartl/klaus.hartl@stilbuero.de
  */
 jQuery.cookie = function (key, value, options) {
+
     
     // key and at least value given, set cookie...
     if (arguments.length > 1 && String(value) !== "[object Object]") {
-        var default_options = {expires: 30, secure: true};
-        options = jQuery.extend(default_options, options);
+        var default_options = {expires: 30};
+        options = jQuery.extend(options, default_options)
 
         if (value === null || value === undefined) {
             options.expires = -1;
