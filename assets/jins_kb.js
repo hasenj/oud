0752992c7829034699aad46773572d421c94c85f
Hasen el Judy <hasan.aljudy@gmail.com>
2013-01-10 06:50:50 -0800

    wip! putting in the instrument's components

--- assets/jins_kb.js
+++ assets/jins_kb.js
@@ -32,3 +32,73 @@
 
  */
 
+ButtonGroup = function(watarJins, index, intervals) {
+    var self = this;
+
+    self.noteName = watarJins.noteName.add(index);
+    self.keys = ko.computed(function() {
+        var keys = [];
+        for(var i = 0; i < intervals.length; i++) {
+            keys.push(new Key(watarJins.baseNote().addRatio(intervals[i])));
+        }
+        return keys;
+    }
+}
+
+// WatarJins = Tetrachord String
+// a series of groups of a buttons that represent "frets" on this string
+WatarJins = function(baseNote, noteName) {
+    var self = this;
+
+    self.baseNote = baseNote;
+    self.noteName = noteName;
+
+    self.groups = [
+        new ButtonGroup(self, 0, [intervals.identity]), // First: identity 
+        new ButtonGroup(self, 1, [intervals.semitone, intervals.neutralSecond, intervals.tone]), // seconds: semitone, neutral, tone
+        new ButtonGroup(self, 2, [intervals.minorThird, intervals.majorThird]),
+        new ButtonGroup(self, 3, [intervals.diminishedForth, intervals.forth]),
+    ];
+
+    self.nextJins = function() {
+        return new WatarJins(self.baseNote().add(intervals.fifth), noteName.add(4));
+    }
+
+}
+
+// AwtarDiwan = Octave Strings
+// a series of Tetrachord strings, separated by a perfect fifth 3:2
+AwtarDiwan = function(baseNote, noteName) {
+    var self = this;
+    self.baseNote = baseNote;
+    self.noteName = noteName;
+    var first = new WatarJins(self.baseNote, self.noteName);
+    self.ajnas = ko.observableArray([first, first.nextJins()]); // start with 2 ajnas
+
+    // previous diwan on the keyboard/intstrument
+    self.prev = ko.observable(null);
+
+    self.index = ko.computed(function() {
+        if(prev()) {
+            return prev().index() + prev().count();
+        } else {
+            return 0
+        }
+    });
+
+    // how many awtar we have; i.e. self.ajnas.length
+    self.count = ko.computed(function() {
+        return self.ajnas().length;
+    });
+
+    self.addJins = function() {
+        self.ajnas.push(self.ajnas().last().nextJins());
+    }
+
+    self.removeJins = function() {
+        if(self.ajnas().length <= 2) {
+            return; // refuse to become less than 2 ajnas
+        }
+        self.ajnas.pop();
+    }
+}
--- assets/ratios.js
+++ assets/ratios.js
@@ -48,7 +48,27 @@ RatioCtor = function(a, b) {
             parts.push(Ratio(cur, cur-step));
             cur -= step;
         }
+        // assert cur == nbot;
         return parts
+    }
+
+    // divergent split
+    self.dsplit = function() {
+        var args = Array.prototype.slice.call(arguments);
+        var sumargs = args.reduce(function(a, b) { return a + b; });
+        var mul_factor = sumargs / (self.a - self.b);
+
+        var ntop = self.a * mul_factor;
+        var nbot = self.b * mul_factor;
+        var parts = [];
+        var cur = ntop;
+        while(args.length) {
+            var step = args.shift();
+            parts.push(Ratio(cur, cur-step));
+            cur -= step;
+        }
+        // assert cur == nbot;
+        return parts;
     }
 
     self.value = function() {
@@ -93,3 +113,38 @@ RatioCtor = function(a, b) {
 Ratio = function(a, b) {
     return new RatioCtor(a, b);
 }
+
+// -------- standard intervals ---------------
+
+var intervals = {
+    identity: Ratio(1, 1),
+    octave: Ratio(2, 1),
+    fifth: Ratio(3, 2)
+    forth: Ratio(4, 3),
+    majorThird: Ratio(5, 4),
+    minorThird: Ratio(6, 5),
+    tone: intervals.fifth.sub(intervals.forth),
+    semitone: intervals.minorThird.sub(intervals.tone),
+    neutralSecond: intervals.minorThird.split(2)[0], // XXX assuming the bigger part comes first!! this should be Ratio(12, 11) maybe we should have "ratio_max" function
+    diminishedForth: intervals.minorThird.add(intervals.semitone),
+}
+
+
+// -------------------------------------------------------------------------------------------
+// -------------------------    Notes   ------------------------------------------------------
+// -------------------------------------------------------------------------------------------
+
+Note = function(frequency) {
+    // assert 0 <= index < 7
+    var self = this;
+    self.frequency = frequency;
+    self.freq = function() { // interface
+        return self.frequency;
+    }
+
+    // apply ratio to a note and come up with a new note
+    self.addRatio = function(ratio) {
+        return new Note(self.freq() * ratio.value());
+    }
+}
+
