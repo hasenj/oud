c04f26bb98c6565f72c1460ef6e88710c75cee80
Hasen el Judy <hasan.aljudy@gmail.com>
2013-07-14 21:38:40 -0600

    don't use flash policy server
    - If your browser doesn't support websockets, I don't care!
        - Yes, we don't have a use for sockets yet anyway, but even if we
          did, I wouldn't care about providing a flash fallback
    - This suppresses errors about not being able to start it anwyay because
      another server is using it or whatever

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from socketio.server import SocketIOServer
 def run_gevent_server():
     port = int(os.getenv("PORT", "9040"))
     # XXX we're not using socket.io ...
-    SocketIOServer(('', port), app, resource="socket.io").serve_forever()
+    SocketIOServer(('', port), app, resource="socket.io", policy_server=False).serve_forever()
 
 def main():
     run_with_reloader(run_gevent_server)
